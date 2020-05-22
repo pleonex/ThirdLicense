e9a590b9fc7a09b271dbfe722908f07766e29954
Benito Palacios Sanchez <benito356@gmail.com>
2020-05-22 23:08:59 +0200

    :tada: Create project

---
+++ src/ThirdLicense/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_third_license
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
