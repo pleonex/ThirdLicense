ca957e5c3a35576aa041f72a7cc0784c52dc6967
Benito Palacios Sanchez <benito356@gmail.com>
2020-05-23 03:32:22 +0200

    :sparkles: Implement license text generator

--- src/ThirdLicense/LicenseTextGenerator.cs
+++ src/ThirdLicense/LicenseTextGenerator.cs
@@ -1,15 +1,68 @@
 namespace ThirdLicense
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Threading.Tasks;
 
     public class LicenseTextGenerator
     {
-        public Task<Stream> Generate(IAsyncEnumerable<PackageInfo> packages)
+        public async Task Generate(Stream output, IAsyncEnumerable<PackageInfo> packages)
         {
-            return Task.FromException<Stream>(new NotImplementedException());
+            var writer = new StreamWriter(output);
+            await foreach (var package in packages) {
+                await WritePackageNotice(writer, package);
+            }
+        }
+
+        private async Task WritePackageNotice(StreamWriter writer, PackageInfo package)
+        {
+            await writer.WriteAsync("License notice for ");
+            await writer.WriteAsync(package.Id.Name);
+            await writer.WriteAsync(" (v");
+            await writer.WriteAsync(package.Id.Version);
+            await writer.WriteLineAsync(")");
+
+            int dotLength = "License notice for ".Length + package.Id.Name.Length;
+            await writer.WriteLineAsync(new string('-', dotLength));
+
+            if (!string.IsNullOrEmpty(package.RepositoryUrl)) {
+                await writer.WriteLineAsync();
+                await writer.WriteAsync(package.RepositoryUrl);
+                if (!string.IsNullOrEmpty(package.RepositoryCommit)) {
+                    await writer.WriteAsync(" at ");
+                    await writer.WriteAsync(package.RepositoryCommit);
+                }
+
+                await writer.WriteLineAsync();
+            }
+
+            if (!string.IsNullOrEmpty(package.ProjectUrl) && package.RepositoryUrl != package.ProjectUrl) {
+                await writer.WriteLineAsync();
+                await writer.WriteLineAsync(package.ProjectUrl);
+            }
+
+            if (!string.IsNullOrEmpty(package.Copyright)) {
+                await writer.WriteLineAsync();
+                await writer.WriteLineAsync(package.Copyright);
+            }
+
+            if (!string.IsNullOrEmpty(package.LicenseExpression)) {
+                await writer.WriteLineAsync();
+                await writer.WriteAsync("Licensed under ");
+                await writer.WriteLineAsync(package.LicenseExpression);
+            }
+
+            if (!string.IsNullOrEmpty(package.LicenseUrl)) {
+                await writer.WriteLineAsync();
+                await writer.WriteLineAsync("Available at");
+                await writer.WriteLineAsync(package.LicenseUrl);
+            } else if (!string.IsNullOrEmpty(package.LicenseContent)) {
+                await writer.WriteLineAsync();
+                await writer.WriteLineAsync(package.LicenseContent);
+            }
+
+            await writer.WriteLineAsync();
+            await writer.WriteLineAsync();
         }
     }
 }
--- src/ThirdLicense/Program.cs
+++ src/ThirdLicense/Program.cs
@@ -9,16 +9,20 @@
         static async Task Main(string[] args)
         {
             string project = "";
+            string endpoint = "https://www.nuget.org/api/v2/";
+            string output = "THIRD-PARTY-NOTICES.TXT";
 
             var analyzer = new DotnetListStdoutAnalyzer();
             var dependencies = analyzer.Analyze(project);
 
-            var nugetInspector = new NuGetInspector();
-            var packages = dependencies.Select(d => nugetInspector.Inspect(d));
+            var nugetInspector = new NuGetV2Inspector(endpoint);
+            var packages = dependencies
+                .SelectAwait(d => nugetInspector.InspectAsync(d))
+                .Where(x => x != null);
 
-            using var outputStream = new FileStream("", FileMode.Open);
+            using var outputStream = new FileStream(output, FileMode.Create);
             var licenseGenerator = new LicenseTextGenerator();
-            await licenseGenerator.Generate(packages);
+            await licenseGenerator.Generate(outputStream, packages);
         }
     }
 }
