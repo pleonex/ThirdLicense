5f53a410f2389110be1e9575a0a1bf1a6a5f00a4
Benito Palacios Sanchez <benito356@gmail.com>
2020-05-23 03:53:13 +0200

    :sparkles: Implement CLI

--- src/ThirdLicense/Program.cs
+++ src/ThirdLicense/Program.cs
@@ -1,17 +1,41 @@
 ﻿namespace ThirdLicense
 {
+    using System.CommandLine;
+    using System.CommandLine.Invocation;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
 
     class Program
     {
-        static async Task Main(string[] args)
-        {
-            string project = "";
-            string endpoint = "https://www.nuget.org/api/v2/";
-            string output = "THIRD-PARTY-NOTICES.TXT";
+        const string NugetEndpointV2 = "https://www.nuget.org/api/v2/";
+        const string DefaultOutputName = "THIRD-PARTY-NOTICES.TXT";
 
+        static Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand("Generate transitive third-party license notice") {
+                new Option<string>("--project") {
+                    Description = "Project file to analyze third-parties",
+                    Required = true,
+                },
+                new Option<string>("--endpoint", () => NugetEndpointV2) {
+
+                    Description = "NuGet repository endpoint (v2 only)",
+                    Required = false,
+                },
+                new Option<string>("--output", () => DefaultOutputName) {
+                    Description = "Path to the output file",
+                    Required = true,
+                },
+            };
+
+            rootCommand.Handler = CommandHandler.Create<string, string, string>(Generate);
+
+            return rootCommand.InvokeAsync(args);
+        }
+
+        static async Task<int> Generate(string project, string endpoint, string output)
+        {
             var analyzer = new DotnetListStdoutAnalyzer();
             var dependencies = analyzer.Analyze(project);
 
@@ -23,6 +47,8 @@
             using var outputStream = new FileStream(output, FileMode.Create);
             var licenseGenerator = new LicenseTextGenerator();
             await licenseGenerator.Generate(outputStream, packages);
+
+            return 0;
         }
     }
 }
